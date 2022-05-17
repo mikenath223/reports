b33639e03d560eee258a07bdc93fb959eae3840f
Mike <michgoldennathan17@gmail.com>
2022-05-17 23:12:12 +0100

    Create reports templates tests

---
+++ src/pages/Reports/Reports.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "app/store";
+import Reports from ".";
+
+describe("Topnav", function () {
+  it("should match snapshot", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Reports />
+      </Provider>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
